24b6ffe15798f41d4fddc64d32117d65c0f8bfe5
Marcus Menezes <mvgmenezes@gmail.com>
2019-04-23 22:45:54 -0300

    Incluindo a listagem no state

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Full Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,23 +21,30 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Column(
-          children: [
-            Container(
+        body: Column(children: [
+          Container(
               margin: EdgeInsets.all(10.0),
               child: RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  //dizendo pro flutter que estou alterando o state
+                  setState(() {
+                    _products.add('Advanced full tester');
+                  });
+                  
+                },
                 child: Text('Add Product'),
-              )
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food Paradaise')
-                ],
-              ),
-            )
+              )),
+          Column(
+              children: _products
+                  .map((elementItem) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(elementItem)
+                          ],
+                        ),
+                      ))
+                  .toList())
         ]),
       ),
     );
